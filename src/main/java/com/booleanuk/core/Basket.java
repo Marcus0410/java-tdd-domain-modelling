bdd02e3f91ac566892d0d423ef86acef5cd963c0
Marcus Ikdal <marcus.ikdal@gmail.com>
2025-01-09 10:33:38 +0100

    implemented add

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -1,4 +1,19 @@
 package com.booleanuk.core;
 
+import java.util.HashMap;
+
 public class Basket {
+    private HashMap<String, Integer> items;
+
+    public Basket() {
+        items = new HashMap<>();
+    }
+
+    boolean add(String product, int price) {
+        if (items.containsKey(product))
+            return false;
+
+        items.put(product, price);
+        return true;
+    }
 }
